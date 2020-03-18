eca59c44650dd7d31735b09e2ad71f0544b93bcd
Bhuwan Upadhyay <bhuwan.upadhyay@clusus.com>
2020-03-18 10:12:47 +0545

    wip

--- shipments-api-java8/src/test/java/io/github/bhuwanupadhyay/shipmentsapijava8/LocalShipmentServiceContractTests.java
+++ shipments-api-java8/src/test/java/io/github/bhuwanupadhyay/shipmentsapijava8/LocalShipmentServiceContractTests.java
@@ -6,14 +6,15 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
+import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties.StubsMode;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
-@AutoConfigureStubRunner(ids = {"io.github.bhuwanupadhyay:orders-api-java8:+:stubs:6569"}, stubsMode = StubsMode.LOCAL)
+@AutoConfigureStubRunner(ids = {"io.github.bhuwanupadhyay:orders-api-java8:+:stubs:6569"},
+        stubsMode = StubRunnerProperties.StubsMode.LOCAL)
 public class LocalShipmentServiceContractTests {
 
     @Autowired
