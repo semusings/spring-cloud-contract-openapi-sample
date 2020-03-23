ab6d0a08652a138e3ca396d369e1265c941c3d08
Bhuwan Upadhyay <bhuwan.upadhyay@clusus.com>
2020-03-23 06:56:39 +0545

    wip

--- orders-api-server/src/test/java/io/github/bhuwanupadhyay/ordersapijava8/application/OrdersApiBase.java
+++ orders-api-server/src/test/java/io/github/bhuwanupadhyay/ordersapijava8/application/OrdersApiBase.java
@@ -1,6 +1,7 @@
 package io.github.bhuwanupadhyay.ordersapijava8.application;
 
 
+import io.github.bhuwanupadhyay.ordersapijava8.domain.EntityNotFoundException;
 import io.github.bhuwanupadhyay.ordersapijava8.domain.OrderEntity;
 import io.github.bhuwanupadhyay.ordersapijava8.domain.OrderRepository;
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
@@ -10,6 +11,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 
+import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 
@@ -21,29 +23,39 @@ class OrdersApiBase {
     }
 
     private OrderRepository stubbedOrderRepository() {
-        OrderEntity orderEntity = new OrderEntity("0a818933-087d-47f2-ad83-2f986ed087eb",
-                "customerId1", "itemName1", 5);
+
+        List<OrderEntity> entities = Lists.list(
+                new OrderEntity(
+                        "0a818933-087d-47f2-ad83-2f986ed087eb",
+                        "Alpha",
+                        "Hot Burger",
+                        5
+                )
+        );
 
         return new OrderRepository() {
-
             @Override
             public Optional<OrderEntity> find(String orderId) {
-                return Optional.ofNullable(Objects.equals(orderId, orderEntity.getOrderId()) ? orderEntity : null);
+                return entities
+                        .stream()
+                        .filter(orderEntity -> Objects.equals(orderId, orderEntity.getOrderId()))
+                        .findFirst();
             }
 
             @Override
             public Page<OrderEntity> list(OrderEntity filters, Pageable pageable) {
-                return new PageImpl<>(Lists.list(orderEntity));
+                return new PageImpl<>(entities);
             }
 
             @Override
             public OrderEntity update(String orderId, OrderEntity changed) {
+                OrderEntity orderEntity = find(orderId).orElseThrow(() -> new EntityNotFoundException(orderId));
                 return new OrderEntity(orderEntity.getOrderId(), orderEntity.getCustomerId(), changed.getItemName(), changed.getQuantity());
             }
 
             @Override
             public void delete(String orderId) {
-
+                find(orderId).orElseThrow(() -> new EntityNotFoundException(orderId));
             }
 
             @Override
